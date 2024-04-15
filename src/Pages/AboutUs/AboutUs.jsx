ff3ee57fe321856ea3a3d33b6bf891232c82b736
Almansur342 <abdullahalmansur2002@gmail.com>
2024-04-15 21:10:23 +0600

    first commit

---
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,9 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>This is about us section</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ src/Pages/UpdateProfile/UpdateProfile.jsx
@@ -0,0 +1,9 @@
+const UpdateProfile = () => {
+  return (
+    <div>
+      <h1>This is update Profile page</h1>
+    </div>
+  );
+};
+
+export default UpdateProfile;
